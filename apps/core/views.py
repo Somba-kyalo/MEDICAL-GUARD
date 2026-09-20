72dce02b25a76d00e7c15500959ec16c89f40453
somba-kyalo <musombasamuel3@gmail.com>
2026-09-21 00:27:03 +0300

    Add Core views

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'core/home.html')
+
+
+def about(request):
+    return render(request, 'core/about.html')
+
+
+def dashboard(request):
+    return render(request, 'core/dashboard.html')
+
+
+def error_page(request):
+    return render(request, 'core/error.html')
