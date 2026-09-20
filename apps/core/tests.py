1a2f20321742bab71da3a6bb3f1afd53c4ee889a
somba-kyalo <musombasamuel3@gmail.com>
2026-09-21 00:52:33 +0300

    Add Core tests

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class CoreViewsTests(TestCase):
+
+    def test_home_page(self):
+        response = self.client.get(reverse('core:home'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/home.html')
+
+    def test_about_page(self):
+        response = self.client.get(reverse('core:about'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/about.html')
+
+    def test_dashboard_page(self):
+        response = self.client.get(reverse('core:dashboard'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/dashboard.html')
+
+    def test_error_page(self):
+        response = self.client.get(reverse('core:error'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/error.html')
