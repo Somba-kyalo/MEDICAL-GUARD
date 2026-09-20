149e5c64969c3a960bcf575a6020a18875e6e83a
somba-kyalo <musombasamuel3@gmail.com>
2026-09-21 00:30:10 +0300

    Add Core URL routing

---
+++ apps/core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'core'
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('error/', views.error_page, name='error'),
+]
