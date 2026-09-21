17d4c1d9969669c70ad7d7e1cfd8e8a076e91c56
somba-kyalo <musombasamuel3@gmail.com>
2026-09-21 23:26:14 +0300

    Add PatientsApp admin

--- apps/PatientsApp/admin.py
+++ apps/PatientsApp/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Patient
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'user',
+        'date_of_birth',
+        'sex',
+        'phone_number',
+        'created_at',
+        'updated_at',
+    )
+
+    list_filter = (
+        'sex',
+        'created_at',
+    )
+
+    search_fields = (
+        'user__username',
+        'user__email',
+        'phone_number',
+        'emergency_contact_name',
+        'emergency_contact_phone',
+    )
+
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
