8e3e779fe88c9343dc122ba65ceb1c79f867cca5
somba-kyalo <musombasamuel3@gmail.com>
2026-09-21 12:53:54 +0300

    Integrate AccountsApp URLs

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
+    path('accounts/', include('apps.AccountsApp.urls')),
 ]
