a378fbbb3f80565ab36d8b604ce1270be7ee8e87
somba-kyalo <musombasamuel3@gmail.com>
2026-09-21 12:31:45 +0300

    Set up AccountsApp user model

--- apps/AccountsApp/models.py
+++ apps/AccountsApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        PATIENT = 'PATIENT', 'Patient'
+        CLINICIAN = 'CLINICIAN', 'Clinician'
+        FACILITY_STAFF = 'FACILITY_STAFF', 'Facility Staff'
+        ADMIN = 'ADMIN', 'Administrator'
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.PATIENT,
+    )
+
+    def __str__(self):
+        return self.username
