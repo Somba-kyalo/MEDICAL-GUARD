68db151242d27b2cdaeb4a76d5979ec39647cbd0
somba-kyalo <musombasamuel3@gmail.com>
2026-09-21 23:44:15 +0300

    Integrate PatientsApp URLs

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
     path('accounts/', include('apps.AccountsApp.urls')),
+    path('patients/', include('apps.PatientsApp.urls')),
 ]
