78c1ee1fd7c0adbe11a4870984df70cfd17293e7
somba-kyalo <musombasamuel3@gmail.com>
2026-09-22 14:15:41 +0300

    Integrate ScreeningApp URLs

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.core.urls')),
-    path('accounts/', include('apps.AccountsApp.urls')),
-    path('patients/', include('apps.PatientsApp.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("apps.core.urls")),
+    path("accounts/", include("apps.AccountsApp.urls")),
+    path("patients/", include("apps.PatientsApp.urls")),
+    path("screening/", include("apps.ScreeningApp.urls")),
 ]
